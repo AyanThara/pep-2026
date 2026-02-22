510d6dde5bb1ae07398bbddb2670a8d68adfce83
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-02-22 22:52:52 +0530

    Update 3523Leetcode.cpp

--- 3523Leetcode.cpp
+++ 3523Leetcode.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int maximumLength(vector<int>& nums) {
+    int maximumPossibleSize(vector<int>& nums) {
         vector<int> st;  
         for(int x : nums){
             int cur = x;
             while(!st.empty() && st.back() > cur){
                 cur = max(cur, st.back());
                 st.pop_back();
-            }   
+            }
             st.push_back(cur);
-        }   
+        }
         return st.size();
     }
 };
